353a470c045cec40114a20338fa61fd9d8d43abc
Ross Cohen <rosscohen1990@gmail.com>
2023-04-10 19:19:22 -0400

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -25,10 +25,4 @@ const nextConfig = {
   },
 };
 
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
+module.exports = nextConfig;
